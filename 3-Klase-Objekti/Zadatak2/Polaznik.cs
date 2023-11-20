2525cd0fc5f2f21dc75ec97a52145a7945442494
Andrija Merlin <105887989+nilrema@users.noreply.github.com>
2023-11-20 01:58:18 +0100

    Create Polaznik.cs

---
+++ 3-Klase-Objekti/Zadatak2/Polaznik.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_2.Models
+{
+    internal class Polaznik
+    {
+        public string? Ime { get; set; }
+        public string? Prezime { get; set; }
+        public string?  Oib { get; set; }
+    }
+}
